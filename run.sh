51d2824c53433f9337101e0358293cb8722e9d18
danakj <danakj@chromium.org>
2025-10-24 10:44:26 -0400

    require prod or staging mode

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 MODE=$1
-if [[ "$MODE" != "staging" ]]; then
+if [[ "$MODE" == "prod" ]]; then
   echo "PROD mode"
   export FIZZ_CONFIG_DIR=$HOME/s/fizz/prod
   export DISCORD_TOKEN=`sh prod/SECRETS`
-else
+elif [[ "$MODE" == "staging" ]]; then
   echo "STAGING mode"
   export FIZZ_CONFIG_DIR=$HOME/s/fizz/staging
   export DISCORD_TOKEN=`sh staging/SECRETS`
+else
+  echo "Specify prod or staging mode."
+  exit 1
 fi
 	 
 cargo run
