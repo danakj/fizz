25e7486b631921197552073346bea3a855ee6c9c
danakj <danakj@chromium.org>
2025-10-24 10:50:53 -0400

    Don't hardcode the path to the fizz repo

--- run.sh
+++ run.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+if ! test -e $PWD/run.sh; then
+  echo "Run this from the fizz bot's root directory."
+  exit 1
+fi
+
 MODE=$1
 if [[ "$MODE" == "prod" ]]; then
   echo "PROD mode"
-  export FIZZ_CONFIG_DIR=$HOME/s/fizz/prod
+  export FIZZ_CONFIG_DIR=$PWD/prod
   export DISCORD_TOKEN=`sh prod/SECRETS`
 elif [[ "$MODE" == "staging" ]]; then
   echo "STAGING mode"
-  export FIZZ_CONFIG_DIR=$HOME/s/fizz/staging
+  export FIZZ_CONFIG_DIR=$PWD/staging
   export DISCORD_TOKEN=`sh staging/SECRETS`
 else
   echo "Specify prod or staging mode."
   exit 1
 fi
-	 
+
+echo "Using config $FIZZ_CONFIG_DIR/fizz.toml"
 cargo run
