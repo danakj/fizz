93bbeefbd156ac03b07cf38a01936b1937582950
danakj <danakj@chromium.org>
2025-10-24 10:42:39 -0400

    Support a separate staging botin run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ MODE=$1
 if [[ "$MODE" != "staging" ]]; then
   echo "PROD mode"
   export FIZZ_CONFIG_DIR=$HOME/s/fizz/prod
+  export DISCORD_TOKEN=`sh prod/SECRETS`
 else
   echo "STAGING mode"
   export FIZZ_CONFIG_DIR=$HOME/s/fizz/staging
+  export DISCORD_TOKEN=`sh staging/SECRETS`
 fi
 	 
-export DISCORD_TOKEN=`sh SECRETS`
 cargo run
